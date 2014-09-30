74aac6ea666681c4d563d077fbb2dbe0ae2b99fe
Pritpal Singh <prit.17may@ymail.com>
2014-10-01 01:33:42 +0530

    a

--- js/blog.js
+++ js/blog.js
@@ -1,14 +1,34 @@
 $(function () {
 
+  var log = function (e, cls) {
+    cls = typeof cls === 'boolean' ? cls : false;
+    cls ? console.clear() : '';
+    console.log(e);
+  };
+
   Parse.$ = jQuery;
 
   // Replace this line with the one on your Quickstart Guide Page
   Parse.initialize("Uc0fPrKn30pHtuiVk20k7nRpC9ASoL9GJkjEkYOc", "Q6BWNsggHT7o6qgd5Izk9qzo0WGfj8QCrVCiZNAq");
 
-  var TestObject = Parse.Object.extend("TestObject");
-  var testObject = new TestObject();
-  testObject.save({foo: "bar"}).then(function (object) {
-    alert("yay! it worked");
+//  var TestObject = Parse.Object.extend("TestObject");
+//  var testObject = new TestObject();
+//  testObject.save({foo: "bar"}).then(function (object) {
+//    alert("yay! it worked");
+//  });
+
+  var Blog = Parse.Object.extend("Blog");
+  var Blogs = Parse.Collection.extend({
+    model: Blog
   });
 
+  var blogs = new Blogs();
+  blogs.fetch({
+    success: function (blogs) {
+      log(blogs);
+    },
+    error: function (blogs, error) {
+      log(error);
+    }
+  });
 });
