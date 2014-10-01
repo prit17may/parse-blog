a1d5f576b62e7b8fc44c1fa83daec4596e37934c
Pritpal Singh <prit.17may@ymail.com>
2014-10-01 08:32:34 +0530

    a

--- js/blog.js
+++ js/blog.js
@@ -21,24 +21,24 @@ $(function () {
   var Blogs = Parse.Collection.extend({
     model: Blog
   });
-  log(Blogs);
+  var BlogsView = Parse.View.extend({
+    template: Handlebars.compile($('#blogs-tpl').html()),
+    render: function () {
+      var collection = {blog: this.collection.toJSON()};
+      this.$el.html(this.template(collection));
+    }
+  });
 
-//  var blogs = new Blogs();
-//  blogs.fetch({
-//    success: function (blogs) {
-//      log(blogs);
-//    },
-//    error: function (blogs, error) {
-//      log(error);
-//    }
-//  });
+  var blogs = new Blogs();
+  blogs.fetch({
+    success: function (blogs) {
+      var blogsView = new BlogsView({collection: blogs});
+      blogsView.render();
+      $('.main-container').html(blogsView.el);
+    },
+    error: function (blogs, error) {
+      log(error);
+    }
+  });
 
-  //Blog View
-//  var BlogsView = Parse.View.extend({
-//    template: Handlebars.compile($('#blogs-tpl').html()),
-//    render: function () {
-//      var collection = {blog: this.collection.toJSON()};
-//      this.$el.html(this.template(collection));
-//    }
-//  });
 });
